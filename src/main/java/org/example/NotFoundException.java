13abd20932af0454c54f859da10a5e988f5a4815
Ivan Pozdov <Ivan.pozdov58@gmail.com>
2023-06-06 22:20:34 +0300

    Initial commit

---
+++ src/main/java/org/example/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(int id) {
+        super("Товар с id = " + id + " не найден.");
+    }
+}
---
+++ src/test/java/org/example/ShopRepositoryTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+    @Test
+    public void testRemoveWhenProductExist() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11,"телефон", 30_000);
+        Product product2 = new Product(22, "книга", 3000);
+        Product product3 = new Product(285, "машина", 3_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(22);
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected,actual);
+    }
+    @Test
+    public void testRemoveWhenProductNotExist() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11,"телефон", 30_000);
+        Product product2 = new Product(22, "книга", 3000);
+        Product product3 = new Product(285, "машина", 3_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class,
+        () -> repo.remove(44)
+        );
+    }
+
+}
